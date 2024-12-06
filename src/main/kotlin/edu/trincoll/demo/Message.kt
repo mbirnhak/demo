78281eafe8fe2caa3c030be8aeee1f1b2a22a520
Matthew Birnhak <MattBirnhak@MacBook-Pro-63.local>
2024-12-05 23:45:05 -0500

    Followed directions, not working

---
+++ src/main/kotlin/edu/trincoll/demo/Message.kt
@@ -0,0 +1,10 @@
+package edu.trincoll.demo
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
+
+@Table("MESSAGES")
+data class Message(
+    val text: String,
+    @Id val id: String? = null
+)
---
+++ src/main/kotlin/edu/trincoll/demo/MessageService.kt
@@ -0,0 +1,13 @@
+package edu.trincoll.demo
+
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+
+@Service
+class MessageService(private val db: MessageRepository) {
+    fun findMessages(): List<Message> = db.findAll().toList()
+
+    fun findMessageById(id: String): Message? = db.findByIdOrNull(id)
+
+    fun save(message: Message): Message = db.save(message)
+}
